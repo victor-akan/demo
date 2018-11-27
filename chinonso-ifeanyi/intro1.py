633996027a1419664740c709a8cafb3d3bda201a
Chinonso99 <45196424+Chinonso99@users.noreply.github.com>
2018-11-27 13:17:00 +0100

    chinonso-ifeanyi

---
+++ chinonso-ifeanyi/intro1.py
@@ -0,0 +1,20 @@
+age = 334
+# age = age == 334
+age = 334 == age
+# print(age)
+
+list_age = [age, 334, 23]
+print(list_age)
+print(list_age[0])
+print(list_age[1])
+print(list_age[2])
+print(list_age[-1])
+print(list_age[-2])
+print(list_age[-3])
+
+dict_age = {'ladies': 7, 'guys': 12, 'vast': list_age}
+print(dict_age['ladies'])
+print(dict_age)
+
+door = {'key-left': 'lock', 'key-right': 'open'}
+print(door)
---
+++ chinonso-ifeanyi/jstraining.js
@@ -0,0 +1,10 @@
+let animal = {
+    age: 20,
+    color : "black",
+    run : function () {
+        console.log("Running")
+    }
+};
+
+console.log(animal.age);
+console.log(animal.run);
